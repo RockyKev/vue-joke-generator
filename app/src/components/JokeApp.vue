ae916b0a8576d8c5aaaf1bf36ea861a7a710fdd5
rockykev <kev.rocky@gmail.com>
2021-02-07 22:32:54 -0800

    working API

--- app/src/components/JokeApp.vue
+++ app/src/components/JokeApp.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
     <h1>Data</h1>
-    <div v-if="errored">Sorry something happened. </div>
+    <div v-if="errored">Sorry something happened.</div>
     <div v-else>
-
-      <div v-if="loading">Loading... </div>
-      <div v-else
-           v-for="currency in info"
-           :key="currency.id"
-           class="currency">
-           {{ currency.description }}
-           <!-- <span class="lighten">
-             <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
-           </span> -->
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <p>This is as of {{ info.time.updateduk }}</p>
+        <p v-for="currency in info.bpi" :key="currency.id" class="currency">
+          {{ currency.description }}({{ currency.code }}):
+          {{ changeToDecimal(currency.rate_float, 2) }}
+        </p>
       </div>
-
     </div>
 
     <!-- <p v-if="responseAvailable">JOKE: {{ result.joke }}</p> -->
@@ -24,75 +20,39 @@
 <script>
 // https://www.techiediaries.com/vue-3-fetch-data-rest-api/
 
-import axios from 'axios';
-
 export default {
   name: "JokeApp",
-  data: function() {
+  data: function () {
     return {
       info: null,
       loading: true,
-      errored: false
+      errored: false,
     };
   },
-  // filters: {
-  //   currencydecimal (value) {
-  //     return value.toFixed(2);
-  //   }
-  // },
   mounted() {
-    const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    this.fetchAPIData();
+  },
+  methods: {
+    fetchAPIData() {
+      const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
-    axios
-      .get(url)
-      .then(response => {
-        this.info = response.data.bpi;
-      })
-      .catch(error => {
-        console.log(error);
-        this.error = true;
-      })
-      .finally(() => this.loading = false)
-  }
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+          this.info = response;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.error = true;
+        })
+        .finally(() => (this.loading = false));
+    },
+    changeToDecimal(value, decimal){
+        return value.toFixed(decimal);
+    }
+  },
 };
 
-// methods: {
-//     fetchAPIData() {
-//       this.responseAvailable = false;
-
-//       // const category = "Programming";
-//       // const language = "";
-//       // const flag = "blacklistFlags=nsfw,racist";
-//       // const jokeType = "";
-//       console.log("in fetchAPI Data");
-
-//       // https://v2.jokeapi.dev/joke/Programming,Dark
-//       fetch(`https://v2.jokeapi.dev/joke/Programming,Dark?blacklistFlags=nsfw,racist`)
-//       // fetch(`https://v2.jokeapi.dev/joke/${category}?${flag}`)
-//         .then(response => {
-//           console.log("In then response")
-//           if (response.ok) {
-//             return response.json();
-//           } else {
-//             alert(
-//               "Server returned " + response.status + " : " + response.statusText
-//             );
-//           }
-//         })
-//         .then(response => {
-//           console.log("fetch was successful");
-//           this.result = response.body;
-//           this.responseAvailable = true;
-//         })
-//         .catch(err => {
-//           console.log(err);
-//         });
-//     },
-//   },
-//   mounted: function() {
-//     console.log("HI THERE");
-//     this.fetchAPIData();
-//   }
 </script>
 
 <style>
